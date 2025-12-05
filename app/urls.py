f517db2aa43d061986d100f57f3cff1393d7f3b6
Aleah <abean26@basecampcodingacademy.org>
2025-12-05 12:07:10 -0600

    changes

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', landing_view, name="home"),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .forms import *
+from .models import *
 
 # Create your views here.
+def landing_view(request:HttpRequest)->HttpResponse:
+    return render (request, "landing.html")
